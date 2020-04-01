633d5740fd5ef4b6080f6eec1479b989fa1c9f3d
EgorZarubin <egorzarubin@gmail.com>
2020-04-01 11:56:41 +0300

    add plots and public transport simulation

--- pandemic.py
+++ pandemic.py
@@ -5,16 +5,18 @@ import cv2
 
 print ("Pandemic simulation")
 
+WRITE_VIDEO = False
+
 class Human:
     virus_state = 0
     virus_days = 0
     age = 30    
-    immunitet = 50
+    immunity = 50
 
-    def __init__(self, virus_state, age, immunitet):
+    def __init__(self, virus_state, age, immunity):
         self.virus_state = virus_state
         self.age = age
-        self.immunitet = immunitet
+        self.immunity = immunity
 
 #init population
 x_size = 500
@@ -25,14 +27,21 @@ population_count = len(population) * len(population[0])
 
 for i in range(len(population)):
     for j in range(len(population[i])):
-        population[i][j] = Human(0, random.randint(1, 80), random.randint(0, 100)) #add correct distribution
+        imm = random.gauss(0.5, 0.1)
+        age = random.randint(1, 80)
+        population[i][j] = Human(0, age, imm) #add correct distribution
         #population[i][j].age = random.randint(1, 80)
-        #population[i][j].immunitet = random.randint(0, 100)
+        #population[i][j].immunity = random.randint(0, 100)
 
 #start epidemy
+
 infected = []
-dead = 0
-healed = 0
+dead = []
+healed = []
+
+infected_dynamics = []
+healed_dynamics = []
+dead_dynamics = []
 
 for h in range(100):
     id = random.randint(0, population_count)
@@ -43,44 +52,55 @@ for h in range(100):
 def spred_virus(id, new_infected):
     spread = random.randint(0,100)
     person = population[id//x_size][id%y_size]
-    spread *= (100 - person.immunitet) / 100 
+    spread *= (1 - person.immunity) 
     if spread > 50 and person.virus_state == 0:
         population[id//x_size][id%y_size].virus_state = 1
         new_infected.append(id)
         #print ("new virus case")
+
+def random_spread(contacts, new_infected):
+    i = 0
+    while i < contacts:
+        id = random.randint(0, x_size*y_size - 1)
+        x = id//x_size
+        y = id%y_size
+        if population[x][y].virus_state == 0:
+            spread = random.randint(0, 100)
+            spread*= (1-population[x][y].immunity)
+            if spread > 90:
+                population[x][y].virus_state = 1
+                new_infected.append(id)
+        i += 1
+
         
 
-def heal(id, healed, dead):
-    if population[id//x_size][id%y_size].virus_state == 1:
+def heal(id):
+    x = id//x_size
+    y = id%y_size
+    if population[x][y].virus_state == 1:
         death = random.randint(0, 1000)
-        death *= population[id//x_size][id%y_size].age / 80
+        death *= population[x][y].age / 80
         if death > 900:
-            population[id//x_size][id%y_size].virus_state = 3
+            population[x][y].virus_state = 3
             infected.remove(id)
-            dead += 1
+            dead.append(id)
         else:
-            population[id//x_size][id%y_size].virus_days += 1
-            if population[id//x_size][id%y_size].virus_days > 15:
-                population[id//x_size][id%y_size].virus_state = 2
-                if id in infected:
-                    infected.remove(id)
-                else:
-                    print("warning: id", id)
-                healed += 1
+            population[x][y].virus_days += 1
+            if population[x][y].virus_days > 15:
+                population[x][y].virus_state = 2
+                infected.remove(id)
+                healed.append(id)
 
-    return healed, dead
+    
 
-
-
-#epydemi step
-plt.figure(figsize=(10,10))
 virus_img = np.ndarray([x_size, y_size, 3], dtype=np.uint8)
 virus_img[:,:] = [255, 255, 255]
-plt.imshow(virus_img)
 
-out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 15, (x_size, y_size))
 
-def update_view(video_out):   
+if WRITE_VIDEO:
+    out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 15, (x_size, y_size))
+
+def update_view(video_out = None):   
     for i in range(len(virus_img)):
         for j in range(len(virus_img)):
             if population[i][j].virus_state == 0:
@@ -93,14 +113,14 @@ def update_view(video_out):
                 virus_img[i,j,2] = 0
             elif population[i][j].virus_state == 3:
                 virus_img[i,j,:] = 0
-                        
-    video_out.write(virus_img)
+    if video_out:               
+        video_out.write(virus_img)
     #plt.imshow(virus_img)
     #plt.draw()
     #plt.pause(0.002)
 
 for step in range(500):
-    print ("step:", step, "number of infected", len(infected), "healed",healed, "dead", dead)
+    print ("step:", step, "number of infected", len(infected), "healed",len(healed), "dead", len(dead))
     new_infected = []
     for id in infected:
         if population[id//x_size][id%y_size].virus_days < 5:
@@ -111,14 +131,32 @@ for step in range(500):
                     n_id = n_id_x *x_size + n_id_y 
                     if id != n_id:
                         spred_virus(n_id, new_infected)
-        healed, dead = heal(id, healed, dead)
+            random_spread(100, new_infected)
+        heal(id)
+        
     for new_id in new_infected:
         infected.append(new_id)
+
+    infected_dynamics.append(len(infected))
+    healed_dynamics.append(len(healed))
+    dead_dynamics.append(len(dead))
+
     if len(infected) == 0:
         break
 #update picture
-    update_view(out)
+    if WRITE_VIDEO:
+        update_view(out)
 
-out.release()
+if WRITE_VIDEO:
+    out.release()
 
+#plot graphics
+figure1 = plt.figure()
+plt.plot(infected_dynamics, 'red')
+plt.plot(healed_dynamics, 'green')
+plt.plot(dead_dynamics, 'black')
+figure2 = plt.figure()
+update_view()
+plt.imshow(virus_img)
+plt.show()
     
